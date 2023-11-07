38b4beb1cc1d5f5774e7ea466c292b81c07e4d7f
Michał Pawlak <michal.pawlak001@gmail.com>
2023-11-07 14:45:39 +0100

    start handling wrong inputs

--- script.js
+++ script.js
@@ -2,16 +2,38 @@ const btn = document.getElementById("form-button");
 const yearsSpan = document.getElementById("years-number")
 const monthsSpan = document.getElementById("months-number")
 const daysSpan = document.getElementById("days-number")
+const daysInputEl = document.getElementById("day");
+const monthsInputEl = document.getElementById("month");
+const yearsInputEl = document.getElementById("year");
+
+daysInputEl.addEventListener("keypress",(e)=>{
+  if((daysInputEl.value.length==2)||(isNaN(daysInputEl.value)))
+  {
+    e.preventDefault();
+    daysInputEl.classList.add("invalid-input");
+  }
+})
+
+daysInputEl.addEventListener("focus",()=>{
+  if(daysInputEl.classList.contains("invalid-input")){
+    daysInputEl.classList.remove("invalid-input")
+  }
+})
 
 btn.addEventListener("click", () => {
-  let day = document.querySelector("#day").value;
-  let month = document.querySelector("#month").value;
-  let year = document.querySelector("#year").value;
-
-
+  let day = daysInputEl.value;
+  let month = monthsInputEl.value;
+  let year = yearsInputEl.value;
 
   let birthDate = new Date();
   let currentTime = new Date();
+
+  if(!day||!month||!year||(+day>31)||(+month>12)||(+year<1970)||(year>currentTime.getFullYear())){
+    alert("Please enter a valid date after January 1st 1970");
+    return 0;
+  }
+
+
 
   birthDate.setFullYear(year,month-1,day);
 
